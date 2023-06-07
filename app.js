c56f4bd98ad75cd980c1f04985f86795518833a0
mrKORKMAZ <musa.krkmz6638@gmail.com>
2023-06-07 20:58:22 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//? Selectors
+const ekleBtn = document.getElementById("ekle-btn")
+const gelirInput = document.getElementById("gelir-input")
+const ekleFormu = document.getElementById("ekle-formu")
+
+
+//? Variables
+
+//?Events
+
+//? Functions
