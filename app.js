5c3758e24597212005e952907f3116880a029595
mrKORKMAZ <musa.krkmz6638@gmail.com>
2023-06-07 22:22:44 +0300

    configuration

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ const ekleFormu = document.getElementById("ekle-formu")
 //? Sonuc tablosu
 const gelirinizTd = document.getElementById("geliriniz")
 
+//? harcama formu
+const harcamaFormu = document.getElementById("harcama-formu")
+const harcamaAlaniInput = document.getElementById("harcama-alani")
+const tarihInput = document.getElementById("tarih")
+const miktarInput = document.getElementById("miktar")
+
 //? Variables
 let gelirler = 0
 
+//? tum harcamalari saklayacak dizi (JSON)
+let harcamaListesi = []
+
 //?Events
 
-//? Formun submit butonuna basildiginda
+//! Formun submit butonuna basildiginda
 ekleFormu.addEventListener("submit", (e) => {
   e.preventDefault() //? reload'u engeller
   gelirler = gelirler + Number(gelirInput.value) //? string eklemiyi engelledik
@@ -28,11 +37,37 @@ ekleFormu.addEventListener("submit", (e) => {
 
 //! Sayfa her yuklendikten sonra calisan event
 window.addEventListener("load", () => {
+  //? gelirler bilgisini local storage'dan okuyarak global degiskenimize yaz
   gelirler = Number(localStorage.getItem("gelirler"))
+
+  //? Tarih inputunu bugun deger ile yukle
+  tarihInput.valueAsDate = new Date()
+
+  //? Degisen bilgileri hesapla ve DOM'a bas
   hesaplaVeGuncelle()
 })
 
-//? Functions
+//! harcama formu submit edildiginde calisir
+harcamaFormu.addEventListener("submit", (e) => {
+  e.preventDefault() //? reload'u engelle
+
+  //? yeni harcama bilgileri ile bir obje olusutur
+  const yeniHarcama = {
+    id: new Date().getTime(),
+    tarih: tarihInput.value,
+    alan: harcamaAlaniInput.value,
+    miktar: miktarInput.value,
+  }
+
+  //? yeni harcama objesini diziye ekle
+  harcamaListesi.push(yeniHarcama)
+
+  //? Formdaki verileri sil
+  harcamaFormu.reset()
+  tarihInput.valueAsDate = new Date()
+  console.log(yeniHarcama)
+})
+
 
 const hesaplaVeGuncelle = () => {
   gelirinizTd.innerText = gelirler
